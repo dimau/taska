57d520776ee67980b717bac1af3d189fb8b75757
Dmitrii <dimau777@gmail.com>
2022-10-17 16:12:33 +0400

    Remove unused files

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { store } from "./app/store";
 import App from "./app/App/App";
 import "./normalize.css";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -18,8 +17,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
