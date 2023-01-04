735914c5bc55d609da072a3b99b724d7532e52d4
Dmitrii <dimau777@gmail.com>
2023-01-04 20:03:09 +0400

    Change footer content - only copyright and make it a little bigger

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -6,9 +6,7 @@ export function Footer() {
     <footer className={styles.root}>
       <div className="container">
         <div className={styles.footerContent}>
-          <span>Dmitrii Ushakov, 2022</span>
-          <a href="mailto:dimau777@gmail.com">Send me email</a>
-          <a href="https://t.me/UshakovDmitrii">Send me telegram message</a>
+          <span>©️ Dmitrii Ushakov, 2022</span>
         </div>
       </div>
     </footer>
