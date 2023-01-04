af92516c27b31d07968c09b6e020b02a128792fe
Dmitrii <dimau777@gmail.com>
2023-01-04 19:55:59 +0400

    Refactoring - make common layout (header and footer too) for site and for the service

--- src/app/App/App.tsx
+++ src/app/App/App.tsx
@@ -4,14 +4,13 @@ import styles from "./App.module.css";
 import { HomePage } from "../../pages/HomePage/HomePage";
 import { TaskListPage } from "../../pages/TaskListPage/TaskListPage";
 import { MainLayout } from "../../layouts/MainLayout/MainLayout";
-import { SiteLayout } from "../../layouts/SiteLayout/SiteLayout";
 import { GoogleAuthorizer } from "../../features/authorization/GoogleAuthorizer/GoogleAuthorizer";
 
 export function App() {
   return (
     <div className={styles.root}>
       <Routes>
-        <Route path="/" element={<SiteLayout />}>
+        <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />} />
         </Route>
         <Route path="/tasks" element={<MainLayout />}>
